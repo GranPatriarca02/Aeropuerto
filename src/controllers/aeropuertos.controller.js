b7fed6758f541687ec2c5584ab53904868d8be9a
GranPatriarca02 <charliepsnoscar@gmail.com>
2026-01-21 18:56:28 +0100

    Add: controllers(aeropuerto)

---
+++ src/controllers/aeropuertos.controller.js
@@ -0,0 +1,54 @@
+import { AeropuertosService } from '../services/aeropuertos.service.js';
+import { successResponse } from '../utils/response.js';
+
+const service = new AeropuertosService();
+
+export const aeropuertosController = {
+  async getAll(req, res, next) {
+    try {
+      const aeropuertos = await service.getAll();
+      successResponse(res, aeropuertos, 'Aeropuertos obtenidos exitosamente');
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  async getById(req, res, next) {
+    try {
+      const { id } = req.params;
+      const aeropuerto = await service.getById(id);
+      successResponse(res, aeropuerto, 'Aeropuerto obtenido exitosamente');
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  async create(req, res, next) {
+    try {
+      const aeropuerto = await service.create(req.validatedData);
+      successResponse(res, aeropuerto, 'Aeropuerto creado exitosamente', 201);
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  async update(req, res, next) {
+    try {
+      const { id } = req.params;
+      const aeropuerto = await service.update(id, req.validatedData);
+      successResponse(res, aeropuerto, 'Aeropuerto actualizado exitosamente');
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  async delete(req, res, next) {
+    try {
+      const { id } = req.params;
+      await service.delete(id);
+      successResponse(res, null, 'Aeropuerto eliminado exitosamente');
+    } catch (error) {
+      next(error);
+    }
+  }
+};
